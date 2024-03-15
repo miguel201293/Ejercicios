5d0c7975d5b22e91ecc5dd2967112b3c2e748fce
miguel201293 <miguel.abato.93@gmail.com>
2024-03-15 10:16:48 +0100

    cambios

---
+++ JavaScript/Tutoriales/caracteristicas/metodo_consola.js
@@ -0,0 +1,9 @@
+//Imprimir por pantalla.
+console.log("hello Word")
+// Imprimir errores
+console.error("Error: algo salio mal")
+//Imprimir aviso de mala practica
+console.warn("alguna cosa no esta mal pero se puede mejorar")
+// para saber cuanto tiempo tarda en cargar nuestro scritp
+console.time();
+setTimeout(()=>console.timeEnd(),500);
---
+++ JavaScript/Tutoriales/caracteristicas/tiempoEspera_Intervalo.js
@@ -0,0 +1,1 @@
+setTimeout(()=>console.log("setTimeOut"),2000);
